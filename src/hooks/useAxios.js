3c369c5e056b2510bf51295fb4a0ed75b4acb7fa
Brian Carela <BrianCarela@gmail.com>
2024-01-25 15:36:07 -0500

    Update useAxios.js

--- src/hooks/useAxios.js
+++ src/hooks/useAxios.js
@@ -1,45 +1,60 @@
-import { useState, useEffect } from "react";
-// `npm install axios` before starting the server
-import axios from "axios";
+import React, { useState, useEffect } from 'react';
+import axios from "axios"
 
 function useAxios() {
     // This holds the result of an API call
-  const [data, setData] = useState(null);
-//   This holds the URL that's sent from a component
-  const [url, setUrl] = useState("");
-//   This holds an error status based on the results of an axios search
-  const [error, setError] = useState(null);
-//   This holds the loading status that's sent from a component
-  const [loading, setLoading] = useState(false);
+    const [data, setData] = useState(null)
+    // This can tell other components that the axios call is still working
+    const [loading, setLoading] = useState(false)
+    // This holds the URL from a component; gives us control over when the call occurs
+    const [url, setUrl] = useState("")
+    // This handles an error status based on the results of an axios call (failure to get wanted results)
+    const [error, setError] = useState(false)
 
-  async function customFetch() {
-    try {
-      let payload = await axios.get(url);
+    // OPTIONAL: Set an API key using setAuth()
+    const [auth, setAuth] = useState(null)
 
-      setData(payload.data);
-      setLoading(false);
-      setError(null);
-    } catch (e) {
-      if (e.response.status === 404) {
-        setError("data not found");
-        setData(null);
-        setLoading(false);
-      } else {
-        setError(e.message);
-        setData(null);
-        setLoading(false);
-      }
+    async function customFetch(){
+        try {
+            let payload
+            if(auth){
+                console.log(`auth was used`)
+
+                payload = await axios.get(url, {
+                    headers: {
+                        'Authorization': auth
+                    }
+                })
+            } else {
+                console.log(`auth was not used`)
+                
+                payload = await axios.get(url)
+            }
+
+            setData(payload.data)
+            setLoading(false)
+            setError(null)
+        } catch (error) {
+            if(error.response.status === 404){
+                setError("data could not be found")
+                setData(null)
+                setLoading(false)
+            } else {
+                setError(error.message)
+                setData(null)
+                setLoading(false)
+            }
+        }
     }
-  }
 
-//   When we use setLoading from another component, this useEffect sees the change in state. If loading is true, then it performs the customFetch function. Else, nothing happens.
-  useEffect(() => {
-    if (loading) {
-      customFetch(url);
-    }
-  }, [loading]);
+    // On the component side, use `setLoading(true)` to trigger this hook
+    useEffect(() => {
+        if(loading){
+            customFetch()
+        }
+    }, [loading])
 
-  return [setUrl, data, loading, setLoading, error];
+    return [setUrl, data, loading, setLoading, error, setAuth];
 }
 
 export default useAxios;
